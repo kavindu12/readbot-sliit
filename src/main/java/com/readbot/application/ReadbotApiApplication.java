ce7f5a308a83c5cb08349cc2b877558c08062ad4
kavindu12 <kavindu.amarawansha@gmail.com>
2019-06-23 22:33:11 +0530

    Added new files

---
+++ src/main/java/com/readbot/application/ReadbotApiApplication.java
@@ -0,0 +1,13 @@
+package com.readbot.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadbotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadbotApiApplication.class, args);
+	}
+
+}
